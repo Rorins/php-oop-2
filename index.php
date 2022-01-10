15960e35ef038669aef0bf152ba466f30aa31707
Aurora Grippaudo <aurora268@hotmail.it>
2022-01-10 17:15:57 +0100

    setting up class

--- index.php
+++ index.php
@@ -7,6 +7,25 @@
     <title>Store</title>
 </head>
 <body>
-    
+<!-- Oggi pomeriggio provate ad immaginare alcune classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.
+Strutturare le classi gestendo l’ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti.
+Eseguire poi degli output istanziando oggetti delle varie classi. -->
+
+<?php
+//CLASS PRODUCT
+class Product{
+   //PROPERTY
+    public $name;
+    public $type;
+    public $price;
+//CONSTRUCTOR
+    function __construct($name, $type, $price){
+        $this->name = $name; 
+        $this->type = $type;
+        $this->type = $price;
+    }
+//METHODS
+}
+?>
 </body>
 </html>
